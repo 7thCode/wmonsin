ba85de8c4911f417e80309f9258a22369463be0d
oda <oda.mikio@gmail.com>
2015-09-22 10:41:58 +0900

    Rev 0.3.0.13

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 var gutil = require('gulp-util');
-var ftp = require('gulp-ftp');
+var ftp = require('vinyl-ftp');
 var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
@@ -9,32 +9,38 @@ var rimraf = require('rimraf');
 var less = require('gulp-less');
 var minifycss = require('gulp-minify-css');
 
-gulp.task('copy',['clean'], function () {
+gulp.task('copy', ['clean'], function () {
     return gulp.src(
         [
             'bin/www',
             'app.js',
             'package.json',
             'config/config.json',
+            'config/logs.json',
+            'model/*.js',
             'views/**/*.jade',
             'routes/**/*.js',
+            'logs/*',
             'public/bower.json',
             'public/favicons/*',
             'public/**/*.css',
             'public/**/*.svg',
             'public/**/*.png',
-            'public/font/*'
+            'public/font/*',
+            'public/javascripts/*.js',
+            'public/backend/javascripts/*.js',
+            'public/front/javascripts/*.js'
         ],
         {base: '..'}
     )
-        .pipe(gulp.dest('dest'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('css', function () {
-   return gulp.src('public/**/stylesheets/*.less')
-       .pipe(less())
-       .pipe(minifycss())
-       .pipe(gulp.dest('dest/wmonsin/public'));
+    return gulp.src('public/**/stylesheets/*.less')
+        .pipe(less())
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/wmonsin/public'));
 });
 
 gulp.task('js', function () {
@@ -47,27 +53,45 @@ gulp.task('js', function () {
         {base: '..'}
     )
         .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jsconcat', function () {
+    return gulp.src(
+        [
+            'public/javascripts/*.js',
+            'public/backend/javascripts/*.js',
+            'public/front/javascripts/*.js'
+        ],
+        {base: '..'}
+    )
+        .pipe(uglify())
         .pipe(concat('client.min.js'))
-        .pipe(gulp.dest('dest/wmonsin/public/javascripts'))
+        .pipe(gulp.dest('dist/wmonsin/public/javascripts'))
         .pipe(gulp.dest('public/javascripts'));
 });
 
 gulp.task('clean', function (cb) {
-    rimraf('dest', cb);
+    rimraf('dist', cb);
 });
 
 gulp.task('ftp', function () {
-    return gulp.src('dest/wmonsin/app.js')
-        .pipe(ftp({
-            host: 'seventh-code.com',
-            user: 'oda',
-            pass: 'sakadachi01',
-            port: 22,
-            remotePath:'app/wmonsin'
-        }))
-        .pipe(gutil.noop());
+
+    var conn = ftp.create({
+        host: '',
+        user: '',
+        password: '',
+        port:22,
+        parallel: 1,
+        log: gutil.log
+    });
+
+    return gulp.src(['dist/**'], {base: 'app/wmonsin', buffer: false})
+        .pipe(conn.newer('/'))
+        .pipe(conn.dest('/'));
+
 });
 
-gulp.task('default', ['copy','css','js'], function() {
+gulp.task('default', ['copy', 'css', 'js'], function () {
     console.log('done');
 });
