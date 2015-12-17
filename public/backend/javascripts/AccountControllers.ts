c89bf21739aff4aedff0fa9c3075152b02200c2e
oda <oda.mikio@gmail.com>
2015-12-17 21:31:55 +0900

    ...

--- public/backend/javascripts/AccountControllers.ts
+++ public/backend/javascripts/AccountControllers.ts
@@ -1192,7 +1192,7 @@ controllers.controller('PageEditController', ['$scope', '$state', '$mdDialog', '
                     templateUrl: '/backend/partials/edit/item/picture/picturecreatedialog',
                     targetEvent: null
                 }).then((answer:any):void => { // Answer
-                    var control:IPictureControl = {
+                     CurrentView.Data.Pages[CurrentView.Page].picture[0] = {
                         height: 600,
                         width: 300,
                         path: answer.items.path,
@@ -1201,7 +1201,6 @@ controllers.controller('PageEditController', ['$scope', '$state', '$mdDialog', '
                         name: answer.items.name,
                         label: answer.items.label
                     };
-                    CurrentView.Data.Pages[CurrentView.Page].picture[0] = control;
                 }, ():void => { // Cancel
                 });
             };
@@ -1986,9 +1985,8 @@ controllers.controller('PictureUpdateDialogController', ['$scope', '$mdDialog', 
         $scope.images = [];
 
         $scope.processFiles = (files:any):void => {
-            var filename = files[0].name;
 
-            $scope.items.path = filename;
+            $scope.items.path = files[0].name;
             $scope.images[0] = {};
             var fileReader = new FileReader();
             var image = new Image();
